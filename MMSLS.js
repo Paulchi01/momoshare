786e67f44f35dbeec7f73558150ed5cd0115e2be
Paulchi01 <160368937+Paulchi01@users.noreply.github.com>
2024-02-18 09:52:45 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -21,6 +21,8 @@ const MMSLS = [
 const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=33658411&pid=07ac94824ea3737aee761428649488f3&tid=a59146d2795674df9de3f647195334f9",
+     "https://www.maimemo.com/share/page?uid=33658411&pid=07ac94824ea3737aee761428649488f3&tid=a59146d2795674df9de3f647195334f9",
+     "https://www.maimemo.com/share/page?uid=33658411&pid=07ac94824ea3737aee761428649488f3&tid=a59146d2795674df9de3f647195334f9",
 
 
   ]
