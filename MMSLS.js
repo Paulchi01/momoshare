8ae5032c212fc128bf2a63e46318fa4c66eba3fb
Paulchi01 <160368937+Paulchi01@users.noreply.github.com>
2024-03-20 14:13:09 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=33658411&pid=6865e327ae488f31c3aab791ab225015&tid=a643eab2d42601859dd389618f335e1c",
+    "https://www.maimemo.com/share/page?uid=33658411&pid=d9f3ebf1178e045be56fceee2ec7b562&tid=b7fa6c033868ea7dac10b64fa1a16ab9",
   
   
   ]
